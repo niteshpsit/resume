2d0a5a708ac7907ba07caa0d64367b919d67be1c
Nitesh Jatav <nitesh.jatav@synapse.com>
2017-02-16 14:04:15 +0530

    Provide the connection to the mongodb using mongoose

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // Require Dependencies
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose');
 
 // Initilize the app with express server
 const app = express()
 
+// connnect to the database resume
+mongoose.connect('mongodb://localhost/resume', (error, data) => {
+    // Run only if server is unable to connect to the database with error display
+    if (error)
+        console.log(error)
+});
 /**
  * Define the public folder path that will contain the public file likes images, css, javascript, html 
  **/
@@ -18,6 +25,6 @@ const port = process.env.PORT || 3001
 /**
  * Starting Server At Given Port
  **/
-app.listen(port,()=>{
-    console.log("server started at :",port)
+app.listen(port, () => {
+    console.log("server started at :", port)
 })
